6266f798e80baa9388732a294f166cb4790f7625
hurleyquin9111 <hurleyquin9111@gmail.com>
2024-06-14 22:19:53 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/aditia-portofolio.v2/" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/aditia-portofolio.v2" : "",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
